26fa7a7cfbb1e1601b341d5145815bdca0b16d0d
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2025-12-01 19:25:20 -0300

    Remoção de códigos depreciados

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Toda vez que o código chamar '/windmill-proxy', o Vite redireciona para o Windmill
-      '/windmill-proxy': {
-        target: 'https://app.windmill.dev',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/windmill-proxy/, '')
-      }
-    }
-  }
 })
